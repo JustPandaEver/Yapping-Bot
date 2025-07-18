6ed9f311de117d6a33f69613f14c5616cfd3661a
PandaEver <justpandaever@gmail.com>
2025-07-19 03:25:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import os, requests, base64, time, re, hashlib, sys, subprocess, shutil, tempfile
+import os, requests, base64, time, re, sys, subprocess
 from datetime import datetime
 from dotenv import load_dotenv
+try:
+    from google import genai
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall", "google-genai"])
+    from google import genai
+
+try:
+    import readchar
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall", "readchar"])
+    import readchar
+
 c = requests.session()
 load_dotenv()
-
+ai_client = genai.Client(api_key=os.getenv('GEMINI_KEY'))
 
 class TwitterBot:
     def reply_tweet(self, access, full_reply, tweet_id):
@@ -19,6 +31,14 @@ class TwitterBot:
                 time.sleep(20)
         except Exception as e:
             pass
+
+def self_ai(ser_res):
+    with open("prompt.txt",'r') as f:
+        promp = f.read()
+    fixed= promp.replace("`TEXT`", ser_res)
+    response = ai_client.models.generate_content(model="gemini-2.5-flash", contents=fixed).text
+    return response.replace('*','')
+
 
 def update_refresh_token(new_token):
     lines = []
@@ -45,6 +65,7 @@ def login():
         os.system(f'start {url}')
     else:
         print(f"https://ai.relayer.host/authorize?cid={os.getenv('CLIENT_ID')}")
+        input("Press Enter to input token")
 
 def get_new_token():
     tok = c.get(f"https://ai.relayer.host/refresh_token?refresh={os.getenv('REFRESH_TOKEN')}&cid={os.getenv('CLIENT_ID')}").json()
@@ -63,6 +84,22 @@ def get_tid(user):
     time.sleep(5)
     return ids
 
+def extract_url(input_text):
+    pattern = r"https://x.com/[^/]+/status/\d+"
+    urls = re.findall(pattern, input_text)
+    return urls
+
+def extract_id(url):
+    pattern = r"https://x.com/([^/]+)/status/(\d+)"
+    match = re.search(pattern, url)
+    if match:
+        username = match.group(1)
+        tweet_id = match.group(2)
+        return username, tweet_id
+    else:
+        print(f"No match found for URL: {url}")
+        return None, None
+
 def get_twit(user, projects, skip_check):
     responsed = c.get(f"https://ai.relayer.host/api/tweet/{os.getenv('AI_KEY')}?id={user}").json()
     if responsed['status'] == "Failed":
@@ -80,6 +117,8 @@ def get_twit(user, projects, skip_check):
         if project in text_lower:
             response = c.get(f"https://ai.relayer.host/api/{os.getenv('AI_KEY')}?text={text}").json()
             decoded_msg = (base64.b64decode(response['message']).decode('utf-8'))
+            if (os.getenv('SELF_AI')).lower() == "y":
+                decoded_msg = self_ai(decoded_msg)
             print(f"Tweet: {text}")
             print("=" * 100 + "\n")
             print(f"Reply: {decoded_msg}")
@@ -95,21 +134,6 @@ def get_twit(user, projects, skip_check):
                 return tweet_id, message
     return None, None
 
-def extract_url(input_text):
-    pattern = r"https://x.com/[^/]+/status/\d+"
-    urls = re.findall(pattern, input_text)
-    return urls
-
-def extract_id(url):
-    pattern = r"https://x.com/([^/]+)/status/(\d+)"
-    match = re.search(pattern, url)
-    if match:
-        username = match.group(1)
-        tweet_id = match.group(2)
-        return username, tweet_id
-    else:
-        print(f"No match found for URL: {url}")
-        return None, None
 
 def raid(id, skip_check):
     responsed = c.get(f"https://ai.relayer.host/api/user/tweet/{os.getenv('AI_KEY')}?tweet={id}").json()
@@ -124,6 +148,8 @@ def raid(id, skip_check):
         return None, None
     response = c.get(f"https://ai.relayer.host/api/{os.getenv('AI_KEY')}?text={text}").json()
     decoded_msg = (base64.b64decode(response['message']).decode('utf-8'))
+    if (os.getenv('SELF_AI')).lower() == "y":
+        decoded_msg = self_ai(decoded_msg)
     print(f"Tweet: {text}")
     print("=" * 100 + "\n")
     print(f"Reply: {decoded_msg}")
@@ -167,7 +193,7 @@ def melon_raid(bot):
                     print("Retry Reply")
                     bot.reply_tweet(token, reply_text, ids)
             except Exception as e:
-                time.sleep(10)
+                time.sleep(5)
                 print(f"Error pada username @{user}: {e}")
         print("Doneeee semua serrr")
 
@@ -230,65 +256,52 @@ def reply_twit(bot, usernames):
         print("Selesai, tunggu 3 jam untuk melanjutkan.")
         time.sleep(10800)
 
-def get_file_sha256(file_path):
-    hash_sha256 = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_sha256.update(chunk)
-    return hash_sha256.hexdigest()
-
-def check_update_with_temp_clone():
-    try:
-        temp_dir = tempfile.mkdtemp()
-        result = subprocess.run(
-            ["git", "clone", "--depth", "1", "https://github.com/JustPandaEver/Yapping-Bot.git", temp_dir],
-            capture_output=True, text=True
-        )
-        if result.returncode != 0:
-            print("Gagal clone repo:")
-            print(result.stderr)
-            shutil.rmtree(temp_dir)
-            return
-        temp_main = os.path.join(temp_dir, "main.py")
-        if not os.path.exists(temp_main):
-            print("main.py tidak ditemukan di repo!")
-            shutil.rmtree(temp_dir)
-            return
-        local_hash = get_file_sha256(__file__)
-        temp_hash = get_file_sha256(temp_main)
-
-        print(f"SHA256 lokal: {local_hash}")
-        print(f"SHA256 repo : {temp_hash}")
-        if local_hash == temp_hash:
-            print("Sudah menggunakan versi terbaru.")
-        else:
-            print("Ada update terbaru di repo!")
-            sys.exit()
-    except Exception as e:
-        print(f"Error saat cek update: {e}")
+def simple_menu(options):
+    idx = 0
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("=== Twitter Auto Reply Bot ===\nGithub: JustPandaEver\nX: PandaEverX\n")
+        for i, option in enumerate(options):
+            if i == idx:
+                print(f"=> {option}")
+            else:
+                print(f"  {option}")
+        key = readchar.readkey()
+        if key == readchar.key.UP:
+            idx = (idx - 1) % len(options)
+        elif key == readchar.key.DOWN:
+            idx = (idx + 1) % len(options)
+        elif key == readchar.key.ENTER or key == '\r':
+            return idx
 
 
 def main():
-    print("=== Twitter Auto Reply Bot ===\nGithub: JustPandaEver\nX: PandaEverX\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
     bot = TwitterBot()
     with open("target.txt", "r") as f:
         usernames = [line.strip() for line in f if line.strip()]
+    menu_options = [
+        "Reply tweet",
+        "Melon Full Auto Raid",
+        "Auto Raid",
+        "Self Ai Prompt TEST",
+        "Exit"
+    ]
     while True:
-        print("1. Reply tweet\n2. Melon Full Auto Raid\n3. Auto Raid\n4. Cek Update\n5. Keluar")
-        choices = str(input("Pilih menu: "))
-        if choices == "1":
+        choice = simple_menu(menu_options)
+        print(f"Kamu memilih: {menu_options[choice]}")
+        if choice == 0:
             reply_twit(bot, usernames)
-        elif choices == "2":
+        elif choice == 1:
             melon_raid(bot)
-        elif choices == "3":
+        elif choice == 2:
             auto_raid(bot)
-        elif choices == "4":
-            check_update_with_temp_clone()
-        elif choices == "5":
+        elif choice == 3:
+            print(self_ai(input("Reply TEXT Example: ")))
+            input("Tekan Enter untuk kembali ke menu...")
+        elif choice == 4:
             print("Keluar dari program.")
-            break            
-        else:
-            print("Pilihan tidak valid.")
+            break
 
 if __name__ == "__main__":
     if(os.getenv('REFRESH_TOKEN') == None or os.getenv('REFRESH_TOKEN') =='\n' or os.getenv('REFRESH_TOKEN') ==''):
